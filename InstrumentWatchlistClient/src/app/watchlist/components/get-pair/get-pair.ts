eea8536b58aeb612b4f9f01c46a8add057263c83
omar-elmeteny <omar.elmeteny@gmail.com>
2026-09-22 11:53:50 +0300

    delete the client project

--- InstrumentWatchlistClient/src/app/watchlist/components/get-pair/get-pair.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-get-pair',
-  imports: [],
-  templateUrl: './get-pair.html',
-  styleUrl: './get-pair.scss',
-})
-export class GetPair {}
--- InstrumentWatchlistClient/src/app/watchlist/pages/list/list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-list',
-  imports: [],
-  templateUrl: './list.html',
-  styleUrl: './list.scss',
-})
-export class List {}
